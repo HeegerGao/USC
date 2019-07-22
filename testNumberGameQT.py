e88fb976dd4d471a7e26ae079e6baa3ccc338891
Chongkai Gao <48041218+HeegerGao@users.noreply.github.com>
2019-07-21 17:44:36 -0700

    Update testNumberGameQT.py

--- testNumberGameQT.py
+++ testNumberGameQT.py
@@ -13,7 +13,7 @@ from qt_robot_interface.srv import *
 from qt_gesture_controller.srv import *
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
-from gesture.msg import Res
+from thumb.msg import Res
 #include IMU, button, orthosis
 import rosbag
 
@@ -281,22 +281,32 @@ def choose_behaviors(number):
     #rest
     elif(number == 19):
     #
+        abc =1
         
     
     else:
         print("Please use a correct number!")
 
 
-def gesture_talk():
-    choose_behaviors(random.randint(1, 6))
+def gesture_talk(num):
+    i = 1
+    while i <= num:
+        choose_behaviors(random.randint(1, 6))
+        i = i + 1
     
     
-def gesture_listen():
-    choose_behaviors(random.randint(7, 8))
+def gesture_listen(num):
+    i = 1
+    while i <= num:
+        choose_behaviors(random.randint(7, 8))
+        i = i + 1
     
 
-def gesture_guess():
-    choose_behaviors(random.randint(9, 11))
+def gesture_guess(num):
+    i = 1
+    while i <= num:
+        choose_behaviors(random.randint(9, 11))
+        i = i + 1
 
 
 def gesture_encourage():
@@ -309,49 +319,166 @@ def encourage_score():
     return score
 
 
-
-rospy.init_node('qt_numbergame')
-#initialize publishers
-right_pub = rospy.Publisher('/qt_robot/right_arm_position/command', Float64MultiArray, queue_size=1)
-left_pub = rospy.Publisher('/qt_robot/left_arm_position/command', Float64MultiArray, queue_size=1)
-head_pub = rospy.Publisher('/qt_robot/head_position/command', Float64MultiArray, queue_size=1)
-emotionShow_pub = rospy.Publisher('/qt_robot/emotion/show', String, queue_size=10)
-gesturePlay_pub = rospy.Publisher('/qt_robot/gesture/play', String, queue_size=10)
-speechSay_pub = rospy.Publisher('/qt_robot/speech/say', String, queue_size=10)
-audioPlay_pub = rospy.Publisher('/qt_robot/audio/play', String, queue_size=10)
-
-#wait for publisher connections
-wtime_begin = rospy.get_time()
-while (audioPlay_pub.get_num_connections() == 0 or
-    speechSay_pub.get_num_connections() == 0 or
-    gesturePlay_pub.get_num_connections() == 0 or
-    emotionShow_pub.get_num_connections() == 0 or
-    right_pub.get_num_connections() == 0 or
-    left_pub.get_num_connections() == 0 or
-    head_pub.get_num_connections() == 0 ) :
-    rospy.loginfo("waiting for subscriber connections")
-    if rospy.get_time() - wtime_begin > 5.0:
-        rospy.logerr("Timeout while waiting for subscribers connection!")
-        sys.exit()
-    rospy.sleep(1)
-
-def Res_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
-def listener():
-#listener node to get topic data
-    rospy.init_node('listener', anonymous=True)
-    #set subscribed topics
-    rospy.Subscriber("/thumb_result", Res, Res)
-    #这里加一个while循环就行
-    while(1):
-        #此处添加另外一个线程的代码
+def dictionary_set():
+    #setting up phrase dictionaries
+    #to guess
+    guess_dict = {1: 'Is your number {}?', #2.5 sec
+                    2: 'I guess {}. Did I guess your number?', #5.5 sec
+                    3: 'Ok I think I know your number. Is it {}?', #5.5 sec
+                    4: 'Is {} right?'} #2.5 sec
+    #higher or lower
+    second_dict = {1: 'Hey {} ,is your number higher than mine? Show me yes or no.', #7 sec 
+                    2: 'Oh no I didn’t get it. Did I guess higher than your number {}?', #7 sec
+                    3: 'Hmm is your guess bigger than mine {}?'} #4 sec  
+    #to encourage play during game 
+    encourage_dict = {1:'Good job {}!', #2.5 sec
+                    2:'Hooray! Let\'s play again {}!', #4.5 sec
+                    3:'That was your best one so far! Keep up the good work {}!', #7 sec
+                    4:'I can tell you are trying really hard {}, nice job!', #5 sec
+                    5:'You are getting better at this {}, wow!', #4 sec
+                    6:'I know this is hard {}, keep trying!'} #4.5 sec
+    clarify_dict = {1: 'I didn’t see that {}, could you repeat that answer for me?', #6 sec
+                    2: 'I think that was a {}. If I’m right could you make a thumbs up/down for me?', #6.5 sec
+                    3: 'Could you show me that answer again {}?'} #4 sec 
+    reward_dict = {1: 'Let’s dance.', #2 sec
+                    2: 'I have a joke {}, why did a crocodile marry a chicken? Because crock-o-doodle-doodle is a good last name!', #9.5 sec
+                    3: 'What is your favorite color {}? Mine is blue.', #5.5 sec
+                    4: 'I like playing games with you {}, you’re very fun. Do you like playing with me?'} #8 sec
+    return guess_dict,second_dict,encourage_dict,clarify_dict,reward_dict
 
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+
 
 if __name__=="__main__":
+    guess_dict,second_dict,encourage_dict,clarify_dict,reward_dict = dictionary_set()
+    rospy.init_node('qt_numbergame')
+    #initialize publishers
+    right_pub = rospy.Publisher('/qt_robot/right_arm_position/command', Float64MultiArray, queue_size=1)
+    left_pub = rospy.Publisher('/qt_robot/left_arm_position/command', Float64MultiArray, queue_size=1)
+    head_pub = rospy.Publisher('/qt_robot/head_position/command', Float64MultiArray, queue_size=1)
+    emotionShow_pub = rospy.Publisher('/qt_robot/emotion/show', String, queue_size=10)
+    gesturePlay_pub = rospy.Publisher('/qt_robot/gesture/play', String, queue_size=10)
+    speechSay_pub = rospy.Publisher('/qt_robot/speech/say', String, queue_size=10)
+    audioPlay_pub = rospy.Publisher('/qt_robot/audio/play', String, queue_size=10)
+
+    #wait for publisher connections
+    wtime_begin = rospy.get_time()
+    while (audioPlay_pub.get_num_connections() == 0 or
+        speechSay_pub.get_num_connections() == 0 or
+        gesturePlay_pub.get_num_connections() == 0 or
+        emotionShow_pub.get_num_connections() == 0 or
+        right_pub.get_num_connections() == 0 or
+        left_pub.get_num_connections() == 0 or
+        head_pub.get_num_connections() == 0 ) :
+        rospy.loginfo("waiting for subscriber connections")
+        if rospy.get_time() - wtime_begin > 5.0:
+            rospy.logerr("Timeout while waiting for subscribers connection!")
+            sys.exit()
+        rospy.sleep(1)
+
+    while 1:
+        #game always running, until shutdown by children
+        # exit_msg = rospy.wait_for_message()
+        # if exit_msg.flag == False:
+        once_again = raw_input('play again?') #type a number
+        if once_again == 'no':
+            #game over
+            speechSay_pub.publish("I had a great time with you today. Bye-bye!")
+            choose_behaviors(16)
+            break
+        elif once_again == 'yes':
+            #introduction
+            speechSay_pub.publish("Hello, my name is Q T Robot. What is your name?") #6.5 sec
+            choose_behaviors(16)
+            name = raw_input('What is your name? ')
+            speechSay_pub.publish("Hi   "+name+""",      I would like to play a guessing game with you. 
+            In the game I get to ask you questions, and you get to answer yes or no
+            only by using a thumbs up or a thumbs down gesture with your right arm.
+            Let's practice. Can you show me a thumbs up to say yes?""") #22-25 sec
+            gesture_talk(3)
+            #configuration
+            correctup = raw_input('Was it a good thumbs up? ')
+            if correctup == 'yes':
+            # msg = rospy.wait_for_message("/thumb_result", Res)
+            #
+            # ^ these two lines would be replaced by camera or IMU input, worst case experimenter prompts
+            #we need a time limit they can answer in - 5 sec?
+                speechSay_pub.publish("Awesome! Now can you show me a thumbs down to say no?") #6 sec
+                gesture_talk(1)
+            correctdown = raw_input('Was it a good thumbs down? ')
+            if correctdown == 'yes':
+            # ^ these two lines would be replaced by camera or IMU input, worst case experimenter prompts
+                speechSay_pub.publish("""Cool! During the game, please keep your hand in the 
+                    middle until I ask you a question. That means your thumb is pointing sideways, 
+                    not up or down! Remember to try as hard as you can to show me thumbs up 
+                    or thumbs down, so I can understand if you mean yes or no! If your thumb 
+                    is going the wrong way, just push the red button to move it back to the 
+                    middle. Remember to keep your hands in the middle when you are not answering 
+                    a question.  And just do your best. Can you show me yes if that’s ok?""") #40.5 sec
+                gesture_talk(5)
+            rospy.sleep(5)
+            #initialize variables
+            nocounter = 0
+            yescounter = 0
+            high = 101
+            low = -1
+
+            #play game now
+            speechSay_pub.publish("Let's play now! Please think of a number between 1 and 100.") #6.5 sec
+            gesture_talk(1)
+            rospy.sleep(5)
+            start = input('What is your number? ')#type a number
+            choose_behaviors(3)#a specific gesture, because of time
+            # ^ this line would be replaced by camera or IMU input, worst case experimenter prompts
+            # we should in general have a way to incorporate this in game to track wrong answers
+            speechSay_pub.publish("I'm thinking of your number.") #3 sec
+            gesture_guess(1)
+            while start < 101:
+            #this line should be changed to a while loop without a prompt input
+                half_range = int((high-low)/2)
+                current = half_range+low
+                random_add = random.randrange(-half_range,half_range) #never add on outside of the guessing range
+                random_guess = random.randrange(1,len(guess_dict))
+                QT = current+random_add
+                random_listen = random.randrange(5,9)
+                random_encourage = random.randrange(9,12)
+                random_other = random.randrange(13,17)
+                speechSay_pub.publish(guess_dict[random_guess].format(QT))            
+                gesture_talk(1)
+                val = raw_input('Is my guess correct?')
+                # ^ this line would be replaced by camera or IMU input, worst case experimenter prompts
+                    # use # number = sys.stdin.readline() if prompt
+                    # number.split()[0]
+                if val == 'no':
+                    speechSay_pub.publish(second_dict[random_guess].format(name))
+                    gesture_talk(1)
+                    nocounter += 1
+                    while True:
+                        val2 = input('higher(1) or lower(0)? ')
+                        # ^ this line would be replaced by camera or IMU input, worst case experimenter prompts
+                            # use # number = sys.stdin.readline() if prompt
+                        if val2 == 1:
+                            low = QT
+                            yescounter += 1
+                            break
+                        if val2 == 0:
+                            high = QT
+                            nocounter += 1
+                            break
+                        else:
+                            print("wrong input!please input again.")
+                else:
+                    speechSay_pub.publish('Hooray I got it! Thanks for playing with me. Let’s play again!') #8 sec
+                    #should they play a minimum of 3 games mandatory, the rest optional?
+                    choose_behaviors(13)
+                    yescounter += 1
+                    print("Number of yes: "+str(yescounter)+". Number of no: "+str(nocounter))
+                    print('I got it!')
+                    break
+        else:
+            print("wrong input!please input again.")
+
+
 
 
 
